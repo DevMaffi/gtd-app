0addf953aabe6b4d50fb58acf273d0da461aaef8
devmaffi <devmaffi550@gmail.com>
2022-09-05 18:34:23 +0300

    Provide project styles structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app'
-import './index.css'
+import './styles/index.sass'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
