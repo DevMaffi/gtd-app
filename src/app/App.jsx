d33d5c4376c1a18780a682a50c5afc59f0687ed9
devmaffi <devmaffi550@gmail.com>
2022-09-06 10:54:32 +0300

    Prettify App component layout

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,34 @@
 function App() {
   return (
     <div className="container grid">
-      <h1 className="text-title">GTD App</h1>
-      <div className="box bg-grade text-title">.bg-grade</div>
-      <div className="box bg-border text">.bg-border</div>
-      <div className="box bg-container text">.bg-container</div>
-      <div className="box bg-container-light text">.bg-container-light</div>
-      <div className="box bg-container-lighten text">.bg-container-lighten</div>
-      <div className="box bg-container-dark text">.bg-container-dark</div>
+      <h1 className="text-title">GTD App Design System</h1>
+
+      <div className="box bg-grade text-title" style={{ padding: '1rem' }}>
+        .bg-grade
+      </div>
+
+      <div className="box bg-border text" style={{ padding: '1rem' }}>
+        .bg-border
+      </div>
+
+      <div className="box bg-container text" style={{ padding: '1rem' }}>
+        .bg-container
+      </div>
+
+      <div className="box bg-container-light text" style={{ padding: '1rem' }}>
+        .bg-container-light
+      </div>
+
+      <div
+        className="box bg-container-lighten text"
+        style={{ padding: '1rem' }}
+      >
+        .bg-container-lighten
+      </div>
+
+      <div className="box bg-container-dark text" style={{ padding: '1rem' }}>
+        .bg-container-dark
+      </div>
     </div>
   )
 }
