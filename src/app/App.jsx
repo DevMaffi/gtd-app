0b7d5bd186b855f370292c1a61b050cbb2e9570f
devmaffi <devmaffi550@gmail.com>
2022-09-06 10:44:09 +0300

    Add color utility classes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,13 @@
 function App() {
   return (
-    <div className="container">
-      <h1>GTD App</h1>
+    <div className="container grid">
+      <h1 className="text-title">GTD App</h1>
+      <div className="box bg-grade text-title">.bg-grade</div>
+      <div className="box bg-border text">.bg-border</div>
+      <div className="box bg-container text">.bg-container</div>
+      <div className="box bg-container-light text">.bg-container-light</div>
+      <div className="box bg-container-lighten text">.bg-container-lighten</div>
+      <div className="box bg-container-dark text">.bg-container-dark</div>
     </div>
   )
 }
