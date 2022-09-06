880d57deb6f9e4c845f7e59b6a84de8b1d5d46f8
devmaffi <devmaffi550@gmail.com>
2022-09-06 18:28:11 +0300

    Config typography design system

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,32 +1,29 @@
 function App() {
   return (
     <div className="container grid">
-      <h1 className="text-title">GTD App Design System</h1>
+      <h1 className="fs-900 text-title">GTD App Design System</h1>
 
       <div className="box bg-grade text-title" style={{ padding: '1rem' }}>
         .bg-grade
       </div>
 
-      <div className="box bg-border text" style={{ padding: '1rem' }}>
+      <div className="box bg-border" style={{ padding: '1rem' }}>
         .bg-border
       </div>
 
-      <div className="box bg-container text" style={{ padding: '1rem' }}>
+      <div className="box bg-container" style={{ padding: '1rem' }}>
         .bg-container
       </div>
 
-      <div className="box bg-container-light text" style={{ padding: '1rem' }}>
+      <div className="box bg-container-light" style={{ padding: '1rem' }}>
         .bg-container-light
       </div>
 
-      <div
-        className="box bg-container-lighten text"
-        style={{ padding: '1rem' }}
-      >
+      <div className="box bg-container-lighten" style={{ padding: '1rem' }}>
         .bg-container-lighten
       </div>
 
-      <div className="box bg-container-dark text" style={{ padding: '1rem' }}>
+      <div className="box bg-container-dark" style={{ padding: '1rem' }}>
         .bg-container-dark
       </div>
     </div>
