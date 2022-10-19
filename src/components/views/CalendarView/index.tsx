6c4f32688182bb3b5d1fbeec403eba1154977928
devmaffi <devmaffi550@gmail.com>
2022-10-19 16:36:39 +0300

    Add types to CalendarView

--- src/components/views/CalendarView/index.tsx
+++ src/components/views/CalendarView/index.tsx
@@ -1,7 +1,22 @@
 import { CalendarHeader, CalendarItem } from 'components/calendar'
 import withScrollHeader from 'hoc/withScrollHeader'
+import { TasksResponse } from 'types'
 
-function CalendarView({ date, scrollHeader, tasks, tasksUpdates, onDate }) {
+interface CalendarViewProps {
+  date: Date
+  scrollHeader: boolean
+  tasks: TasksResponse
+  tasksUpdates: number
+  onDate: (date: Date) => void
+}
+
+const CalendarView: React.FC<CalendarViewProps> = ({
+  date,
+  scrollHeader,
+  tasks,
+  tasksUpdates,
+  onDate,
+}) => {
   return (
     <div className="calendar container">
       <CalendarHeader date={date} scrollHeader={scrollHeader} onDate={onDate} />
