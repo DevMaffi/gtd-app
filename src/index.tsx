da5e4f0b2cefd969ee159bf964c3df1f9090a16a
devmaffi <devmaffi550@gmail.com>
2022-10-19 21:35:15 +0300

    Closes #27

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app'
-import './styles/index.sass'
+import App from 'app'
+import 'styles/index.sass'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
