d6c75997979f43d9aaaf46d79ea32436e273bd23
devmaffi <devmaffi550@gmail.com>
2022-10-20 15:51:07 +0300

    Provide types for MonthSelector

---
+++ src/components/month/MonthSelector/MonthSelector.stories.tsx
@@ -0,0 +1,30 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import MonthSelector, {
+  MonthSelectorProps,
+} from 'components/month/MonthSelector'
+import withRootStyles from 'hoc/withRootStyles'
+import { getEnvDate } from 'utils/date'
+
+const StyledMonthSelector = withRootStyles<MonthSelectorProps>(MonthSelector)
+
+export default {
+  title: 'Components/Month/Selector',
+  component: StyledMonthSelector,
+  decorators: [
+    Story => (
+      <div style={{ marginLeft: 25 }}>
+        <Story />
+      </div>
+    ),
+  ],
+  argTypes: {
+    date: { defaultValue: getEnvDate() },
+    onDate: { action: 'clicked', table: { disable: true } },
+  },
+} as ComponentMeta<typeof MonthSelector>
+
+const Story: ComponentStory<typeof MonthSelector> = args => {
+  return <StyledMonthSelector {...args} />
+}
+
+export const Primary = Story.bind({})
---
+++ src/components/month/MonthSelector/index.tsx
@@ -0,0 +1,101 @@
+import { useMemo } from 'react'
+import { DropdownItem as Dropdown } from 'components/dropdown'
+import { MonthSwiper } from 'components/month'
+import { getEnvDate, compareDates } from 'utils/date'
+import data from 'data.json'
+
+export interface MonthSelectorProps {
+  date: Date
+  onDate: (date: Date) => void
+}
+
+export type DropdownView = 'year' | 'month' | null
+export type Options = string | number
+
+const MonthSelector: React.FC<MonthSelectorProps> = ({ date, onDate }) => {
+  const yearOptions = useMemo<number[]>(() => {
+    const now = new Date(getEnvDate())
+    const firstYear = now.getFullYear() - 3
+
+    const options: number[] = []
+
+    const range = [...Array(12).keys()]
+    range.forEach(i => options.push(firstYear + i))
+
+    return options
+  }, [])
+  const monthOptions = data.shortenings.months
+
+  const getOptions = (dropdownView: DropdownView): Options[] => {
+    if (dropdownView === 'year') return yearOptions
+    return monthOptions
+  }
+
+  const getIsActiveHandler = (dropdownView: DropdownView) => {
+    if (dropdownView === 'year')
+      return (yearIndex: number) =>
+        yearOptions[yearIndex] === date.getFullYear()
+
+    return (monthIndex: number) => monthIndex === date.getMonth()
+  }
+
+  const getDefaultOption = (dropdownView: DropdownView) => {
+    const now = new Date(getEnvDate())
+
+    if (dropdownView === 'year')
+      return (yearIndex: number) => yearOptions[yearIndex] === now.getFullYear()
+
+    return (monthIndex: number) => {
+      const optionDate = new Date(
+        date.getFullYear(),
+        monthIndex,
+        date.getDate()
+      )
+      return compareDates(optionDate, now)
+    }
+  }
+
+  const selectMonth =
+    (
+      dropdownView: DropdownView,
+      onDropdown: (dropdownView: DropdownView) => void
+    ) =>
+    (optionIndex: number) =>
+    () => {
+      const nextDate = new Date(
+        date.getFullYear(),
+        date.getMonth(),
+        date.getDate()
+      )
+
+      if (dropdownView === 'month') {
+        nextDate.setMonth(optionIndex)
+        onDropdown(null)
+      } else {
+        nextDate.setFullYear(yearOptions[optionIndex])
+        onDropdown('month')
+      }
+
+      onDate(nextDate)
+    }
+
+  return (
+    <Dropdown
+      on={({ isOpen, dropdownView, onDropdown }) => (
+        <MonthSwiper
+          date={date}
+          isOpen={isOpen}
+          dropdownView={dropdownView}
+          onDropdown={onDropdown}
+          onDate={onDate}
+        />
+      )}
+      isActive={getIsActiveHandler}
+      isDefault={getDefaultOption}
+      setOptions={getOptions}
+      onSelect={selectMonth}
+    />
+  )
+}
+
+export default MonthSelector
